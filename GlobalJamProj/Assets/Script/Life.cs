f73b87284216df16016b25a4fa34e4253c425c65
fernando-oliv <fernandooliveira@estudante.ufscar.br>
2023-02-04 03:39:31 -0300

    mudanças no sistema de vida, game over e mudança de fase

--- GlobalJamProj/Assets/Script/Life.cs
+++ GlobalJamProj/Assets/Script/Life.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Life : MonoBehaviour
 {
@@ -9,6 +11,9 @@ public class Life : MonoBehaviour
     public float recuoy;
     public float recuox;
     private float recuoxbkp;
+    public GameObject[] hearts;
+    public GameObject gameOverBackground;
+
     // Start is called before the first frame update
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -18,8 +23,29 @@ public class Life : MonoBehaviour
             { recuoxbkp = -recuox; }
             else { recuoxbkp = recuox; }
             life -= collision.gameObject.GetComponent<Damage>().damage;
+            updateUI(false);
             GetComponent<Rigidbody2D>().AddForce(new Vector3(recuoxbkp, recuoy, 0),ForceMode2D.Impulse);
         }
         
+    }
+
+    void updateUI(bool condition)
+    {
+        //caso chame o metodo como falso, a vida sera diminuida
+        //se chamar como true, aumenta a vida
+        hearts[(int)life].SetActive(condition);
+        if (life == 0)
+        {
+            //call game over function here
+            gameOver();
+            
+        }
+    }
+
+    void gameOver()
+    {
+        Time.timeScale = 0f;
+        gameOverBackground.SetActive(true);
+
     }
 }
