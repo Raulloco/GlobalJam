8d0dea5bedeccc74aac893518d6519b557d4d49c
Raulloco <raul-luar@hotmail.com.br>
2023-02-04 14:15:16 -0300

    Tudo Funfando

---
+++ GlobalJamProj/Assets/Script/NextScene.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string nextSceneName;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(nextSceneName);
+        }
+    }
+}
